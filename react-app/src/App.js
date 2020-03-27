3c1c0504544fa496d3ee3dea10b8c7a72f8ec0a5
Zarina Gumbatova <26606767+Zarina2311@users.noreply.github.com>
2020-03-27 14:58:00 -0700

    Finished the app

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,23 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
-import { getAllBooks } from "./helpers/booktonica-api-fetcher";
+import { getAllBooks, getComments } from "./helpers/booktonica-api-fetcher";
 import BookCardList from "./components/BookCardList";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      books: []
+      books: [],
+      comments: []
     };
   }
 
   componentDidMount() {
     getAllBooks().then(books => this.setState({ books: books }));
+    getComments().then(comments => this.setState({ comments: comments }));
   }
+
   render() {
     return (
       <div className="App">
-        <BookCardList books={this.state.books} />
+        <BookCardList books={this.state.books} comments={this.state.comments} />
       </div>
     );
   }
